b75d489148948f0e2c01c4c4c12ed9061f0cdb9a
playingapi <playingapi@gmail.com>
2025-05-06 10:12:29 +0800

    Update setup-server.sh

--- scripts/setup-server.sh
+++ scripts/setup-server.sh
@@ -152,16 +152,21 @@ echo "root:123qwe!@#" | chpasswd && print_success "Root password changed to '123
 print_step "Unmasking and starting Docker & containerd..."
 
 # 清理 dockerd 进程和文件
+systemctl stop docker.socket
+systemctl stop docker
 pkill -9 -f dockerd 2>/dev/null
+pkill -9 -f docker 2>/dev/null
 rm -f /var/run/docker.pid /var/run/docker.sock
+
+systemctl unmask containerd >/dev/null 2>&1
+systemctl unmask docker >/dev/null 2>&1
+systemctl unmask docker.socket >/dev/null 2>&1
+systemctl enable docker>/dev/null 2>&1
 
 # 确保 containerd 运行
 systemctl start containerd || echo "Failed to start containerd, check 'journalctl -u containerd.service'" >&2
 
-systemctl unmask docker >/dev/null 2>&1
-systemctl unmask docker.socket >/dev/null 2>&1
-systemctl unmask containerd >/dev/null 2>&1
-systemctl enable docker>/dev/null 2>&1
+
 systemctl restart docker && print_success "Docker & containerd restarted" || print_error "Failed to start Docker/containerd"
 
 
