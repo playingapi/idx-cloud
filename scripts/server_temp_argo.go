52cb57b95b90deb11f0807f8d21f5279d2a5a1ac
playingapi <playingapi@gmail.com>
2025-05-11 20:56:28 +0800

    Update server_temp_argo.go

--- scripts/server_temp_argo.go
+++ scripts/server_temp_argo.go
@@ -128,7 +128,9 @@ func runScript(w http.ResponseWriter, r *http.Request) {
 
 
 	fmt.Fprintf(w, "keep alive:<br>")
-	fmt.Fprintf(w, "for ((i=0; i<24*3600; i+=60)); do echo \"Refreshed at $(date)\"; curl -sL alive_url; sleep 60; done\n")
+	fmt.Fprintf(w, "for ((i=0; i<24*3600; i+=60)); do echo \"Refreshed at $(date)\"; curl -sL alive_url; sleep 60; done<br>")
+	fmt.Fprintf(w, "for ((i=0; i<24*3600; i+=60)); do echo \"Refreshed at $(date)\"; curl -sL http://127.0.0.1:9002/alive; sleep 60; done\n")
+
 	// 结束 HTML 页面
 	fmt.Fprintf(w, "</body>\n</html>")
 
