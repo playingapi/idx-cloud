74807ee5f40ee1599443e1dc7a4dad624d74623e
playingapi <playingapi@gmail.com>
2025-05-12 18:15:13 +0800

    Update setup-server.sh

--- scripts/setup-server.sh
+++ scripts/setup-server.sh
@@ -39,28 +39,6 @@ print_step "Changing root password..."
 
 echo "root:123qwe!@#" | chpasswd && print_success "Root password changed to '123qwe!@#'" || print_error "Failed to change root password"
 
-### STEP 4: Docker & containerd ###
-print_step "Unmasking and starting Docker & containerd..."
-
-# 清理 dockerd 进程和文件
-systemctl stop docker.socket
-systemctl stop docker
-pkill -9 -f dockerd 2>/dev/null
-pkill -9 -f docker 2>/dev/null
-rm -f /var/run/docker.pid /var/run/docker.sock
-
-systemctl unmask containerd >/dev/null 2>&1
-systemctl unmask docker >/dev/null 2>&1
-systemctl unmask docker.socket >/dev/null 2>&1
-systemctl enable docker>/dev/null 2>&1
-
-# 确保 containerd 运行
-systemctl start containerd || echo "Failed to start containerd, check 'journalctl -u containerd.service'" >&2
-
-
-systemctl restart docker && print_success "Docker & containerd restarted" || print_error "Failed to start Docker/containerd"
-
-
 
 
 print_step "install zsh"
@@ -399,6 +377,33 @@ fi
 
 print_step "lsof -i :9022"
 lsof -i :9022
+
+sleep 2
+
+### STEP 4: Docker & containerd ###
+print_step "Unmasking and starting Docker & containerd..."
+
+# 清理 dockerd 进程和文件
+systemctl stop docker.socket
+systemctl stop docker
+pkill -9 -f dockerd 2>/dev/null
+pkill -9 -f docker 2>/dev/null
+rm -f /var/run/docker.pid /var/run/docker.sock
+
+systemctl unmask containerd >/dev/null 2>&1
+systemctl unmask docker >/dev/null 2>&1
+systemctl unmask docker.socket >/dev/null 2>&1
+systemctl enable docker>/dev/null 2>&1
+
+# 确保 containerd 运行
+systemctl start containerd || echo "Failed to start containerd, check 'journalctl -u containerd.service'" >&2
+
+
+systemctl restart docker && print_success "Docker & containerd restarted" || print_error "Failed to start Docker/containerd"
+
+sleep 5
+
+
 
 # Prompt the user for confirmation
 #read -p "Do you want to run argosb.sh? (y/N): " response
