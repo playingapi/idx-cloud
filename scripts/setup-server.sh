80517f8d1d4dac65da7f9876e1e1b3f42cae9ae5
playingapi <playingapi@gmail.com>
2025-05-12 16:26:29 +0800

    Update setup-server.sh

--- scripts/setup-server.sh
+++ scripts/setup-server.sh
@@ -34,35 +34,6 @@ print_error() { echo -e "${RED}✖ $1${RESET}"; }
 print_header
 
 
-
-# Find the PID and process name of the process listening on port 22
-PROCESS_INFO=$(lsof -i :9022 -F pc | grep '^p' -A1)
-
-# Extract PID and process name
-PID=$(echo "$PROCESS_INFO" | grep '^p' | cut -d'p' -f2)
-PROCESS_NAME=$(echo "$PROCESS_INFO" | grep '^c' | cut -d'c' -f2)
-
-# Check if PID was found
-if [ -z "$PID" ]; then
-    echo "No process found listening on port 9022."
-else
-    # Print PID and process name
-    echo "Found process: $PROCESS_NAME (PID: $PID)"
-    
-    # Kill the process with SIGKILL (-9)
-    kill -9 "$PID"
-    
-    # Verify if the process was killed
-    if [ $? -eq 0 ]; then
-        echo "Process $PROCESS_NAME with PID $PID has been terminated."
-    else
-        echo "Failed to terminate process $PROCESS_NAME with PID $PID."
-    fi
-
-fi
-
-
-
 ### STEP 3: Change root password ###
 print_step "Changing root password..."
 
@@ -309,10 +280,6 @@ print_step "tailscale ip"
 
 tailscale ip
 
-print_step "lsof -i :9022"
-lsof -i :9022
-
-
 
 ### STEP : Kill SSSD ###
 print_step "Kill SSHD"
@@ -342,6 +309,33 @@ else
     fi
 
 fi
+
+# Find the PID and process name of the process listening on port 9022
+PROCESS_INFO=$(lsof -i :9022 -F pc | grep '^p' -A1)
+
+# Extract PID and process name
+PID=$(echo "$PROCESS_INFO" | grep '^p' | cut -d'p' -f2)
+PROCESS_NAME=$(echo "$PROCESS_INFO" | grep '^c' | cut -d'c' -f2)
+
+# Check if PID was found
+if [ -z "$PID" ]; then
+    echo "No process found listening on port 9022."
+else
+    # Print PID and process name
+    echo "Found process: $PROCESS_NAME (PID: $PID)"
+    
+    # Kill the process with SIGKILL (-9)
+    kill -9 "$PID"
+    
+    # Verify if the process was killed
+    if [ $? -eq 0 ]; then
+        echo "Process $PROCESS_NAME with PID $PID has been terminated."
+    else
+        echo "Failed to terminate process $PROCESS_NAME with PID $PID."
+    fi
+
+fi
+
 
 
 ### Last STEP: SSH Configuration ###
@@ -403,7 +397,8 @@ else
 fi
 
 
-
+print_step "lsof -i :9022"
+lsof -i :9022
 
 # Prompt the user for confirmation
 #read -p "Do you want to run argosb.sh? (y/N): " response
