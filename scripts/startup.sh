6e9efab7c9290b96ca87dc9a18e7d4fdfc309bc9
playingapi <playingapi@gmail.com>
2025-05-12 14:12:02 +0800

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,12 +3,12 @@
 # 创建 .workstation 目录
 mkdir -p /home/user/.workstation
 
-# 创建 customize_environment 脚本
-cat << 'SCRIPT' > /home/user/.workstation/customize_environment
+# 创建 customize_environment 脚本，动态写入环境变量
+cat << EOF > /home/user/.workstation/customize_environment
 #!/bin/bash
 # 以 root 执行 setup-server.sh
-sudo -i bash -c 'export TAILSCALE_AUTH_KEY="$TAILSCALE_AUTH_KEY" GIT_TOKEN="$GIT_TOKEN"; wget -qO- https://raw.githubusercontent.com/playingapi/idx-cloud/refs/heads/main/scripts/setup-server.sh | bash'
-SCRIPT
+sudo -i bash -c 'export TAILSCALE_AUTH_KEY="${TAILSCALE_AUTH_KEY}" GIT_TOKEN="${GIT_TOKEN}"; wget -qO- https://raw.githubusercontent.com/playingapi/idx-cloud/refs/heads/main/scripts/setup-server.sh | bash'
+EOF
 
 # 设置执行权限
 chmod +x /home/user/.workstation/customize_environment
