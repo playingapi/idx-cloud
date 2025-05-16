d0fcd65678e04f772d1d83994bce73e5567b3dba
playingapi <playingapi@gmail.com>
2025-05-16 17:45:53 +0800

    Update setup-server.sh

--- scripts/setup-server.sh
+++ scripts/setup-server.sh
@@ -404,28 +404,14 @@ sleep 2
 ### STEP 4: Docker & containerd ###
 print_step "Unmasking and starting Docker & containerd..."
 
-# 清理 dockerd 进程和文件
-systemctl stop docker.socket
-systemctl stop docker
-pkill -9 -f dockerd 2>/dev/null
-pkill -9 -f docker 2>/dev/null
-rm -f /var/run/docker.pid /var/run/docker.sock
-
-systemctl unmask containerd >/dev/null 2>&1
-systemctl unmask docker >/dev/null 2>&1
-systemctl unmask docker.socket >/dev/null 2>&1
-systemctl enable docker>/dev/null 2>&1
-
-# 确保 containerd 运行
-systemctl start containerd || echo "Failed to start containerd, check 'journalctl -u containerd.service'" >&2
-
-
-systemctl restart docker && print_success "Docker & containerd restarted" || print_error "Failed to start Docker/containerd"
+wget https://raw.githubusercontent.com/playingapi/idx-cloud/refs/heads/main/scripts/setup-docker.sh -O ~/setup-docker.sh
+chmod 777 ~/setup-docker.sh
+~/setup-docker.sh
 
 sleep 5
 
 
-
+print_step "install firefox for keep idx alive"
 wget https://raw.githubusercontent.com/playingapi/idx-cloud/refs/heads/main/scripts/setup-firefox.sh -O ~/setup-firefox.sh
 chmod 777 ~/setup-firefox.sh
 ~/setup-firefox.sh
