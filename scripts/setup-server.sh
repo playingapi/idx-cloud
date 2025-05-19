39173e2d091bd2b80c5ce1905a5db7d2bc47147d
playingapi <playingapi@gmail.com>
2025-05-19 13:17:23 +0800

    Update setup-server.sh

--- scripts/setup-server.sh
+++ scripts/setup-server.sh
@@ -23,4 +23,4 @@ fi
 USER="hhsw2015"
 REPO="idx-cloud"
 
-sudo -i /bin/bash -c "export TS_TAILNET=\"${TS_TAILNET}\" TS_AUTH_KEY=\"${TS_AUTH_KEY}\" TS_API_KEY=\"${TS_API_KEY}\" GIT_TOKEN=\"${GIT_TOKEN}\" USER=\"${USER}\" REPO=\"${REPO}\"; bash <(curl -fsSL --retry 3 --retry-delay 2 -H \"Authorization: token \$GIT_TOKEN\" \"https://raw.githubusercontent.com/\$USER/\$REPO/refs/heads/main/scripts/setup-server.sh\")"
+sudo -i /bin/bash -c "export TS_TAILNET=\"${TS_TAILNET}\" TS_AUTH_KEY=\"${TS_AUTH_KEY}\" TS_API_KEY=\"${TS_API_KEY}\" GIT_TOKEN=\"${GIT_TOKEN}\"; bash <(curl -fsSL --retry 3 --retry-delay 2 -H \"Authorization: token \$GIT_TOKEN\" \"https://raw.githubusercontent.com/${USER}/${REPO}/refs/heads/main/scripts/setup-server.sh\")"
